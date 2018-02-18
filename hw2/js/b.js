945fc267e31e1f5835a4f908cac2ade38c0cd3b8
CarpeSwag <james@garbagnati.com>
2018-02-18 06:20:59 -0800

    Started part B.

---
+++ hw2/js/b.js
@@ -0,0 +1,140 @@
+var renderer, scene, camera;
+var boxOfPoints;
+var WIDTH, HEIGHT;
+
+var tex1 = new THREE.TextureLoader().load( "res/spark.png" );
+
+
+function init() {
+	WIDTH = window.innerWidth;
+	HEIGHT = window.innerHeight;
+	
+	camera = new THREE.PerspectiveCamera( 40, WIDTH / HEIGHT, 1, 10000 );
+	camera.position.z = 300;
+	var controls = new THREE.OrbitControls( camera );
+
+
+	scene = new THREE.Scene();
+
+	//initialize point attributes
+
+	//var amount = 10000;
+	//var radius = 25;
+	var amount = 2000;
+	var radius = 20;
+
+	var positions = new Float32Array( amount * 3 );
+	var colors = new Float32Array( amount * 3 );
+	var sizes = new Float32Array( amount );
+
+	var vertex = new THREE.Vector3();
+	var color = new THREE.Color( 0xffffff );
+
+	for ( var i = 0; i < amount; i ++ ) {
+
+		vertex.x = ( Math.random() * 2 - 1 ) * radius;
+		vertex.y = ( Math.random() * 2 - 1 ) * radius;
+		vertex.z = ( Math.random() * 2 - 1 ) * radius;
+		vertex.toArray( positions, i * 3 );
+
+		if ( vertex.x < 0 ) {
+
+			color.setHSL( 0.5 + 0.1 * ( i / amount ), 0.7, 0.5 );
+			//color.setRGB( 1.0, 0.0, 0.0 );
+
+		} else {
+
+			color.setHSL( 0.0 + 0.1 * ( i / amount ), 0.9, 0.5 );
+			//color.setRGB( 0.0, 0.0, 1.0 );
+		}
+
+		color.toArray( colors, i * 3 );
+
+		sizes[ i ] = 20;
+
+	}
+
+	var geometry = new THREE.BufferGeometry();
+	geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+	geometry.addAttribute( 'customColor', new THREE.BufferAttribute( colors, 3 ) );
+	geometry.addAttribute( 'size', new THREE.BufferAttribute( sizes, 1 ) );
+
+	//
+
+	var material = new THREE.ShaderMaterial( {
+		uniforms: {
+			amplitude: { value: 1.0 },
+			color:     { value: new THREE.Color( 0xffffff ) },
+			texture:   { value: tex1 }
+		},
+		vertexShader:   document.getElementById( 'particle-vs' ).textContent,
+		fragmentShader: document.getElementById( 'particle-fs' ).textContent,
+
+		blending:       THREE.AdditiveBlending,
+		depthTest:      false,
+		transparent:    true
+
+	});
+
+	//
+
+	boxOfPoints = new THREE.Points( geometry, material );
+	scene.add( boxOfPoints );
+
+	//
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( WIDTH, HEIGHT );
+
+	var container = document.getElementById( 'container' );
+	container.appendChild( renderer.domElement );
+
+	//
+
+	window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function animate() {
+
+	requestAnimationFrame( animate );
+
+	render();
+
+}
+
+function render() {
+
+	var time = Date.now() * 0.005;
+
+	boxOfPoints.rotation.z = 0.01 * time;
+
+	var geometry = boxOfPoints.geometry;
+	var attributes = geometry.attributes;
+
+	for ( var i = 0; i < attributes.size.array.length; i++ ) {
+
+		attributes.size.array[ i ] = 14 + 13 * Math.sin( 0.1 * i + time );
+
+	}
+
+	attributes.size.needsUpdate = true;
+
+	renderer.render( scene, camera );
+
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+	init();
+	animate();
+});
